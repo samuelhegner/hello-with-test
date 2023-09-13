dffc59c7cf4d11a328c592e248520f14f03454fd
Samuel Hegner <samuel.hegner@gmail.com>
2023-09-13 15:36:42 +0100

    message supports different languages

--- hello_test.go
+++ hello_test.go
@@ -5,9 +5,58 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Samuel")
-	want := "Hello Samuel!"
+	t.Run("greet individual when name is supplied in english", func(t *testing.T) {
+		got := Hello("Samuel", "english")
+		want := "Hello Samuel!"
 
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("greet world on empty string in english", func(t *testing.T) {
+		got := Hello("", "english")
+		want := "Hello World!"
+
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("greet individual when name is supplied in spanish", func(t *testing.T) {
+		got := Hello("Samuel", "spanish")
+		want := "Hola Samuel!"
+
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("greet world on empty string in spanish", func(t *testing.T) {
+		got := Hello("", "spanish")
+		want := "Hola Mundo!"
+
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("greet individual when name is supplied in french", func(t *testing.T) {
+		got := Hello("Samuel", "french")
+		want := "Bonjour Samuel!"
+
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("greet world on empty string in french", func(t *testing.T) {
+		got := Hello("", "french")
+		want := "Bonjour le monde!"
+
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("default to engish on unknown language", func(t *testing.T) {
+		got := Hello("Sam", "FAKE_LANG")
+		want := "Hello Sam!"
+
+		assertCorrect(t, got, want)
+	})
+}
+
+func assertCorrect(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("expected %q got %q", want, got)
 	}
--- main.go
+++ main.go
@@ -3,9 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello("Samuel"))
+	fmt.Println(Hello("", "french"))
 }
 
-func Hello(name string) string {
-	return "Hello " + name + "!"
+type greetingPair struct {
+	greeting string
+	world    string
+}
+
+var localGreetings = map[string]greetingPair{
+	"spanish": {
+		greeting: "Hola ",
+		world:    "Mundo",
+	},
+	"english": {
+		greeting: "Hello ",
+		world:    "World",
+	},
+	"french": {
+		greeting: "Bonjour ",
+		world:    "le monde",
+	},
+}
+
+func Hello(name string, lang string) string {
+	if _, ok := localGreetings[lang]; !ok {
+		lang = "english"
+	}
+
+	g := localGreetings[lang].greeting
+	w := localGreetings[lang].world
+
+	if name == "" {
+		return g + w + "!"
+	}
+
+	return g + name + "!"
 }
